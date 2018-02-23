10156f11db8f15013146f0f8b5e9994eab40b157
Luke Davis <russell.davis@okstate.edu>
2018-02-22 21:10:42 -0600

    Ready to setup picker

--- FireDynamicsCalculator/Calculator Controllers/FlameHeightViewController.swift
+++ FireDynamicsCalculator/Calculator Controllers/FlameHeightViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FlameHeightViewController: UIViewController {
+class FlameHeightViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +21,74 @@ class FlameHeightViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - DataSource
+    let length: [String] = ["m", "cm", "ft", "mm", "in"]
+    let heat: [String] = ["kW", "Btu / Sec"]
+    let area: [String] = ["m²", "ft²", "m²"]
+    
+    var currentDataSet: [String] = []
 
+    // MARK: - Outlets
+    
+    @IBOutlet weak var HRR_TF: UITextField!
+    @IBOutlet weak var HRR_Units: UIButton!
+    
+    @IBOutlet weak var diameterTF: UITextField!
+    @IBOutlet weak var diameter_Units: UIButton!
+    
+    @IBOutlet weak var area_TF: UITextField!
+    @IBOutlet weak var area_Units: UIButton!
     
     
     
-
+    // MARK: - Buttons
+    
+    @IBAction func changeHRR_Units(_ sender: Any) {
+        currentDataSet = heat
+    }
+    
+    @IBAction func changeDiameter_Units(_ sender: Any) {
+        currentDataSet = length
+    }
+    
+    @IBAction func changeArea_Units(_ sender: Any) {
+        currentDataSet = area
+    }
+    
+    // MARK: - Picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currentDataSet.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currentDataSet[row]
+    }
+    
+    func openPicker() {
+        
+    }
+    
+    func closePicker() {
+        
+    }
+    
+    
+    // MARK: - TextField
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == HRR_TF {
+            diameterTF.becomeFirstResponder()
+        } else if textField == diameterTF {
+            textField.resignFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
+    
 }
--- FireDynamicsCalculator/Conversion.swift
+++ FireDynamicsCalculator/Conversion.swift
@@ -256,6 +256,8 @@ class Conversion {
             
             case Steel
         }
+        
+        
     
     }
     
@@ -267,6 +269,7 @@ extension Double {
     func rounded(toPlaces places:Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
+        
     }
 }
 
