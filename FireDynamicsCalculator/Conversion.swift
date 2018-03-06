86914fbb7889412b181b343436a333b7ff0851a6
Luke Davis <russell.davis@okstate.edu>
2018-03-05 20:05:44 -0600

    Reformatted Conversions file

--- FireDynamicsCalculator/Conversion.swift
+++ FireDynamicsCalculator/Conversion.swift
@@ -9,103 +9,147 @@
 import Foundation
 
 class Conversion {
-    func length(value: Double, from unit: Units.Length) -> Double {
-        switch unit {
-        case .cm:
-            return value * 0.01
-        case .feet:
-            return value * 0.304878049
-        case .inches:
-            return value * 0.025406504
-        case .meters:
-            return value * 1.0
-        case .mm:
-            return value * 0.001
+    struct Length {
+        enum Length {
+            case cm
+            case feet
+            case inches
+            case meters
+            case mm
+        }
+        
+        func getLengthUnits(from: String) -> Length {
+            switch from {
+            case "cm":
+                return .cm
+            case "ft":
+                return .feet
+            case "in":
+                return .inches
+            case "m":
+                return .meters
+            case "mm":
+                return .mm
+            default:
+                return .meters
+            }
+        }
+        
+        func convertLength(value: Double, from unit: Length) -> Double {
+            switch unit {
+            case .cm:
+                return value * 0.01
+            case .feet:
+                return value * 0.304878049
+            case .inches:
+                return value * 0.025406504
+            case .meters:
+                return value * 1.0
+            case .mm:
+                return value * 0.001
+            }
         }
     }
     
-    func getLengthUnits(from: String) -> Units.Length {
-        switch from {
-        case "cm":
-            return .cm
-        case "ft":
-            return .feet
-        case "in":
-            return .inches
-        case "m":
-            return .meters
-        case "mm":
-            return .mm
-        default:
-            return .meters
+    
+    struct Density {
+        enum Density {
+            case kg_m³
+            case lbs_ft³
+        }
+        
+        func getDensityUnits(from string: String) -> Density {
+            switch string {
+            case "kg/m³":
+                return .kg_m³
+            case "lbs/ft³":
+                return .lbs_ft³
+            default:
+                return .kg_m³
+            }
+        }
+        
+        func convertDensity(value: Double, from units: Density) -> Double {
+            switch units {
+            case .kg_m³:
+                return value * 1
+            case .lbs_ft³:
+                return value * 0.062427818
+            }
         }
     }
     
-    func volume(value: Double, from unit: Units.Volume) -> Double {
-        switch unit {
-        case .ftCub:
-            return value * 0.0283168
-        case .gallon:
-            return value * 0.00378541
-        case .inchesCub:
-            return value * 0.0000163870562770569
-        case .liter:
-            return value * 0.001
-        case .metersCub:
-            return value * 1.0
+    struct Energy {
+        func energy(value: Double, from unit: Units.Energy) -> Double {
+            switch unit {
+            case .BtuSec:
+                return value * 1.055055852
+            case .kW:
+                return value * 1.0
+            }
+        }
+        func getEnergyUnits(from: String) -> Units.Energy {
+            switch from {
+            case "Btu / Sec":
+                return .BtuSec
+            case "kW":
+                return .kW
+            default:
+                return .kW
+            }
         }
     }
     
-    func density(value: Double, from unit: Units.Density) -> Double {
-        switch unit {
-        case .kgCubM:
-            return value * 1.0
-        case .lbCubFt:
-            return value * 0.062427818
+    struct EnergyDensity {
+        enum EnergyDensity {
+            case Btu_Sec_ft²
+            case kW_m²
+        }
+        
+        func getEnergyDensityUnits(from: String) -> EnergyDensity {
+            switch from {
+            case "Btu/sec/ft²":
+                return .Btu_Sec_ft²
+            case "kW/m²":
+                return .kW_m²
+            default:
+                return .kW_m²
+            }
+        }
+        
+        func convertEnergyDensity(value: Double, to unit: EnergyDensity) -> Double {
+            switch unit {
+            case .Btu_Sec_ft²:
+                return value * 1/11.3565267
+            case .kW_m²:
+                return value * 1.0
+            }
         }
     }
     
-    func energy(value: Double, from unit: Units.Energy) -> Double {
-        switch unit {
-        case .BtuSec:
-            return value * 1.055055852
-        case .kW:
-            return value * 1.0
+    struct Area {
+        func area(value: Double, from unit: Units.Area) -> Double {
+            switch unit {
+            case .FtSq:
+                return value * 0.092950625
+            case .inchesSq:
+                return value * 0.00064549
+            case .mSq:
+                return value * 1.0
+            }
         }
-    }
-    
-    func getEnergyUnits(from: String) -> Units.Energy {
-        switch from {
-        case "Btu / Sec":
-            return .BtuSec
-        case "kW":
-            return .kW
-        default:
-            return .kW
-        }
-    }
-    
-    func area(value: Double, from unit: Units.Area) -> Double {
-        switch unit {
-        case .FtSq:
-            return value * 0.092950625
-        case .inchesSq:
-            return value * 0.00064549
-        case .mSq:
-            return value * 1.0
-        }
-    }
-    
-    func getAreaUnits(from: String) -> Units.Area {
-        switch from {
-        case "ft²":
-            return .FtSq
-        case "in²":
-            return .inchesSq
-        case "m²":
-            return .mSq
-        default:
-            return .mSq
+        
+        func getAreaUnits(from: String) -> Units.Area {
+            switch from {
+            case "ft²":
+                return .FtSq
+            case "in²":
+                return .inchesSq
+            case "m²":
+                return .mSq
+            default:
+                return .mSq
+            }
         }
     }
     
@@ -120,47 +164,35 @@ class Conversion {
         }
     }
     
-    func energyDensity(value: Double, to unit: Units.EnergyDensity) -> Double {
-        switch unit {
-        case .Btu_Sec_ft²:
-            return value * 1/11.3565267
-        case .kW_m²:
-            return value * 1.0
+    struct Time {
+        enum Time {
+            case Hour
+            case Min
+            case Sec
         }
-    }
-    
-    func getEnergyDensityUnits(from: String) -> Units.EnergyDensity {
-        switch from {
-        case "Btu/sec/ft²":
-            return .Btu_Sec_ft²
-        case "kW/m²":
-            return .kW_m²
-        default:
-            return .kW_m²
+        
+        func time(value: Double, from unit: Time) -> Double {
+            switch unit {
+            case .Hour:
+                return value * 0.000277778
+            case .Min:
+                return value * 0.016666667
+            case .Sec:
+                return value * 1
+            }
         }
-    }
-    
-    func time(value: Double, from unit: Units.Time) -> Double {
-        switch unit {
-        case .Hour:
-            return value * (1/3600)
-        case .Min:
-            return value * (1/60)
-        case .Sec:
-            return value * 1
-        }
-    }
-    
-    func getTimeUnits(string: String) -> Units.Time {
-        switch string {
-        case "Hr":
-            return .Hour
-        case "Min":
-            return .Min
-        case "Sec":
-            return .Sec
-        default:
-            return .Sec
+        
+        func getTimeUnits(string: String) -> Time {
+            switch string {
+            case "Hr":
+                return .Hour
+            case "Min":
+                return .Min
+            case "Sec":
+                return .Sec
+            default:
+                return .Sec
+            }
         }
     }
     
@@ -190,41 +222,49 @@ class Conversion {
         }
     }
     
-    func getTemperatureUnits(string: String) -> Units.Temperature {
-        switch string {
-        case "C":
-            return .C
-        case "F":
-            return .F
-        case "K":
-            return .K
-        case "R":
-            return .R
-        default:
-            return .C
+    struct Temperature {
+        enum Temperature {
+            case C
+            case F
+            case K
+            case R
         }
-    }
-    
-    func convertTemperature(value: String, from units: Units.Temperature) -> Double {
-        if let value = Double(value) {
-            if units == .F {
-                let result = (value - 32) * ( 5  / 9)
-                return result
-            } else if units == .C {
-                return value
-            } else if units == .K {
-                let result = value - 273.15
-                return result
-            } else {
-                let result = (value - 459.67 - 32 ) * (5 / 9)
-                return result
+        
+        func getTemperatureUnits(string: String) -> Temperature {
+            switch string {
+            case "°C":
+                return .C
+            case "°F":
+                return .F
+            case "K":
+                return .K
+            case "R":
+                return .R
+            default:
+                return .C
             }
-        } else {
-            return 0.0
+        }
+        
+        func convertTemperature(value: String, from units: Temperature) -> Double {
+            if let value = Double(value) {
+                if units == .F {
+                    let result = (value - 32) * ( 5  / 9)
+                    return result
+                } else if units == .C {
+                    return value
+                } else if units == .K {
+                    let result = value - 273.15
+                    return result
+                } else {
+                    let result = (value - 459.67 - 32 ) * (5 / 9)
+                    return result
+                }
+            } else {
+                return 0.0
+            }
         }
     }
     
-
     struct Units {
         enum Length {
             case cm
