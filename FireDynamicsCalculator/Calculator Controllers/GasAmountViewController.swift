aadc4db80ac2bdd63fceaddcab589add4273056d
Luke Davis <russell.davis@okstate.edu>
2018-03-13 19:16:02 -0500

    Prepped for Gas Amount

--- FireDynamicsCalculator/Calculator Controllers/GasAmountViewController.swift
+++ FireDynamicsCalculator/Calculator Controllers/GasAmountViewController.swift
@@ -14,6 +14,8 @@ class GasAmountViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
